c0b901dd6ce36602c149a45f253da6456270c402
adeyanjuaina <51255921+adeyanjuaina@users.noreply.github.com>
2023-03-13 18:44:37 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+// https://teachablemachine.withgoogle.com/models/8qVzqaJPA/
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+   png_quality:90
+});
+
+camera=document.getElementById("camera");
+Webcam.attach("camera");
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("results").innerHTML='<img id="capture_image" src="'+data_uri+'"/>';
+    })
+}
+
+console.log("ml5 version",ml5.version);
+
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/8qVzqaJPA/model.json',modelLoaded);
+function modelLoaded(){
+    console.log("modelloaded");
+}
+
+function check(){
+    img=document.getElementById("capture_image");
+    classifier.classify(img,gotresult);
+}
+
+function gotresult(error,results){
+    if(error){
+        console.error(error)
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML=results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(2);
+    }
+}
